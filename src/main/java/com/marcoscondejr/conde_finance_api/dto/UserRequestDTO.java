dd82d1eaafe49708afd4d8c196f8f85d04deea16
marcoscondejr <marcos14conde@hotmail.com>
2026-01-19 01:38:30 -0300

    feat: adicionando validações na requisições do usuário

--- src/main/java/com/marcoscondejr/conde_finance_api/dto/UserRequestDTO.java
+++ src/main/java/com/marcoscondejr/conde_finance_api/dto/UserRequestDTO.java
@@ -1,9 +1,26 @@
 package com.marcoscondejr.conde_finance_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record UserRequestDTO(
+        @NotBlank(message = "O Nome é obrigatório")
         String name,
+
+        @NotBlank(message = "O Email é obrigatório")
+        @Email(message = "Email inválido")
         String email,
+
+        @Pattern(
+                regexp = "\\d{11}",
+                message = "CPF deve conter 11 dígitos"
+        )
         String cpf,
+
+        @NotBlank(message = "A Senha é obrigatória")
+        @Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
         String password
 ) {
 }
