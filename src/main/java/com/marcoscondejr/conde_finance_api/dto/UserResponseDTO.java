f33472a926ded1e516a683b544b7ed5202aba14f
marcoscondejr <marcos14conde@hotmail.com>
2026-01-21 02:43:35 -0300

    wip

--- src/main/java/com/marcoscondejr/conde_finance_api/dto/UserResponseDTO.java
+++ src/main/java/com/marcoscondejr/conde_finance_api/dto/UserResponseDTO.java
@@ -1,9 +1,11 @@
 package com.marcoscondejr.conde_finance_api.dto;
 
+import com.marcoscondejr.conde_finance_api.enums.UserRole;
+
 public record UserResponseDTO(
         String name,
         String email,
         String login,
-        String role
+        UserRole role
 ) {
 }
