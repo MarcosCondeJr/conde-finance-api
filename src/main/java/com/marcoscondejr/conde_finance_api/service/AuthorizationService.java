4759de0291b46e57bcce9d01742cc7af370944b2
marcoscondejr <marcos14conde@hotmail.com>
2026-01-21 01:52:15 -0300

    feat: adicionando o security configurations para sobrescrever o spring security

--- src/main/java/com/marcoscondejr/conde_finance_api/service/AuthorizationService.java
+++ src/main/java/com/marcoscondejr/conde_finance_api/service/AuthorizationService.java
@@ -1,4 +1,18 @@
 package com.marcoscondejr.conde_finance_api.service;
 
-public class AuthorizationService {
+import com.marcoscondejr.conde_finance_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
 }
