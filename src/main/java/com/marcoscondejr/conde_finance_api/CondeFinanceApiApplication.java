fe40fe905a0c6e770b374925f87bb40c566138c1
Marcos Conde <marcos14conde@hotmail.com>
2026-07-16 18:10:47 -0300

    feat: adicionando configuração para o env

--- src/main/java/com/marcoscondejr/conde_finance_api/CondeFinanceApiApplication.java
+++ src/main/java/com/marcoscondejr/conde_finance_api/CondeFinanceApiApplication.java
@@ -3,10 +3,13 @@ package com.marcoscondejr.conde_finance_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marcoscondejr.conde_finance_api.config.DotenvLoader;
+
 @SpringBootApplication
 public class CondeFinanceApiApplication {
 
 	public static void main(String[] args) {
+		DotenvLoader.init();
 		SpringApplication.run(CondeFinanceApiApplication.class, args);
 	}
 
